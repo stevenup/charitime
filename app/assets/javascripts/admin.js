89a5fdccdeb840da8549cd23b25791dc87ddb0ee
NaiveCAI <835010809@qq.com>
2016-04-11 14:48:33 +0800

    fix fk admin js require bug for the timeago not displayed in datatable

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -2,16 +2,15 @@
 //= require jquery_ujs
 //= require lodash/lodash
 //= require common/namespace
+//= require common/utils-common
+//= require common/ajax-injection
+
 //= require simplify_admin/simplify_admin
 //= require datatables/media/js/jquery.dataTables
 //= require datatables/media/js/dataTables.bootstrap
+//= require common/utils-datatables
+
 //= require bootstrap-modal/js/bootstrap-modalmanager
 //= require bootstrap-modal/js/bootstrap-modal
 //= require integrations/bootstrap-modal-bs3-fix
-//= require common/utils-datatables
-//= require common/ajax-injection
-//= require common/utils-common
-
-//= require integrations/datatables_turbolinks_patch
-
 //= require ckeditor/init
