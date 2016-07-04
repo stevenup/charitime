8cb427b0dcd7887f02ca12d7d76fb4f33d1ac74c
NaiveCAI <835010809@qq.com>
2016-07-04 08:06:51 +0800

    麻痹啊

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -15,6 +15,4 @@
 //= require integrations/bootstrap-modal-bs3-fix
 //= require ckeditor/init
 //= require js-routes
-//= require custom/wechat/wx_config
-//= require custom/wechat/wx_pay.js.coffee
 //= require jquery_ujs
