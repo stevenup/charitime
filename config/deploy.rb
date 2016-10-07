7ebebf61d61437ee909324ec4dea65b257cb3542
Steven <289979456@qq.com>
2016-10-07 15:59:17 +0800

    fix deploy.rb for unicorn restart.

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :keep_releases, 5
 
 set :linked_files, %w{config/database.yml config/application.yml config/secrets.yml config/wechat.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
