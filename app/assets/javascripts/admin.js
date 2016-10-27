f775faf8f1ba149a93645dda3964a5016b2e0cd6
Steven <289979456@qq.com>
2016-10-27 17:44:08 +0800

    MIMO - Attempted to fix the problem that the datetimepicker didn't work.

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -12,10 +12,9 @@
 //= require bootstrap-modal/js/bootstrap-modalmanager
 //= require bootstrap-modal/js/bootstrap-modal
 //= require integrations/bootstrap-modal-bs3-fix
+//= require datetimepicker/build/jquery.datetimepicker.full.min
 //= require ckeditor/init
 
 //= require common/ajax-injection
 //= require common/notifications-injection
 //= require common/utils-common
-
-//= require datetimepicker/build/jquery.datetimepicker.full.min
--- app/helpers/statuses_helper.rb
+++ app/helpers/statuses_helper.rb
@@ -13,8 +13,8 @@ module StatusesHelper
     else
       return '退款驳回'    if order_status == 'REFUND_REJECTED'
       return '退款处理中'  if order_status == 'REFUNDING'
-      return '已退款'      if order_status == '-REFUNDED'
-      return '支付取消'    if order_status == '-CANCELED'
+      return '已退款'      if order_status == 'REFUNDED'
+      return '支付取消'    if order_status == 'CANCELED'
       return '支付失败'    if order_status == 'FAILED'
       return '未支付'      if order_status == 'UNPAID'
       'NaN'
