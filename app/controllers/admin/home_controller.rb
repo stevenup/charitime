7bfac812d4bbd3f585f5f925bc7ac5ec888b4820
Steven <289979456@qq.com>
2017-01-05 18:30:02 +0800

    IMPT - Finished the sum up in the dashboard.

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,4 +1,9 @@
 class Admin::HomeController < Admin::AuthenticatedController
   def index
+    @num_undelivered_orders = Order.where('order_status = ?', 1).count
+    @num_refunding_orders   = Order.where('order_status = ?', -4).count
+    @sales_volume           = Order.where('order_status = ?', 1).sum(:total_price)
+    @num_new_users          = User.where('created_at between ? and ?', Time.new.change(hour: 0), Time.now).count
+    @num_supports           = Support.where('status = ?', '1').count
   end
 end
