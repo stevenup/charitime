48fb72c3713eaceed974631d27f1eb6eab21de67
Steven <289979456@qq.com>
2017-04-09 22:13:55 +0800

    IMPT - Improved the auth user logic 2.

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -1,20 +1,42 @@
 module SessionHelper
-  def log_in code, au_type
+  def log_in(code, au_type)
     Rails.logger.info '********** in log_in method **********'
     openid = Modules::Wechat.get_user_openid code
-    unless openid.nil?
+    if openid
+      if au_type.nil?
+        info = Modules::Wechat.get_user_info_sns_base openid
+      else
+        access_token = Modules::Wechat.get_access_token
+        info = Modules::Wechat.get_user_info_sns_userinfo(openid, access_token)
+      end
       user = User.find_by(:openid => openid)
       if user.nil?
-        info = Modules::Wechat.get_user_info_sns_user_info openid unless au_type.nil?
-        info = Modules::Wechat.get_user_info_sns_base openid if au_type.nil?
         user = User.new info
         user.save
       end
-      session[:user_id] = user.id
+      session[:user_id] = user.id if user
     else
-      raise 'Get openid error.'
+      Rails.logger.info '******************** Get openid error ********************.'
     end
     Rails.logger.info '********** leave log_in method **********'
+  end
+
+  def update_user(code, au_type)
+    openid = Modules::Wechat.get_user_openid code
+    if openid
+      if au_type.nil?
+        info = Modules::Wechat.get_user_info_sns_base openid
+      else
+        access_token = Modules::Wechat.get_access_token
+        info = Modules::Wechat.get_user_info_sns_userinfo(openid, access_token)
+      end
+      user = User.find_by(:openid => openid)
+      if user and info
+        user.update_attributes(info) if user['nickname'] != info['nickname'] or user['headimgurl'] != info['headimgurl']
+      end
+    else
+      Rails.logger.info '******************** Get openid error ********************.'
+    end
   end
 
   def current_user
@@ -23,6 +45,6 @@ module SessionHelper
   end
 
   def logged_in?
-    !current_user.nil?
+    current_user.nil?
   end
 end
