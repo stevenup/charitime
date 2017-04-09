913e7ee4124e12dc1ff601aa2428ff4a71185ee7
Steven <289979456@qq.com>
2017-04-09 15:43:24 +0800

    MIMO - Changed all auth type to snsapi_base.

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -4,9 +4,7 @@ class BaseController < ApplicationController
 
   # 如果是分享出去的连接，拼url时加上au_type参数，用于判断oauth为sns_userinfo
   def auth_user
-    Rails.logger.info '###############################################'
-    Rails.logger.info 'Starting to auth user.'
-    Rails.logger.info '###############################################'
+    Rails.logger.info '###################### Starting to auth user #########################'
     code    = params[:code]
     au_type = params[:type]
     if logged_in?
