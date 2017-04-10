505952da1ba02d15fe9773f66326a878d4af0057
Steven <289979456@qq.com>
2017-04-10 22:47:46 +0800

    FNCR - Finished the wx sharing apis of homepage and index && detail page of shelf_items and projects pages.

--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -40,8 +40,8 @@ module SessionHelper
   end
 
   def current_user
-    @current_user ||= User.find_by(:id => session[:user_id])
-    # @current_user ||= User.last
+     @current_user ||= User.find_by(:id => session[:user_id])
+     # @current_user ||= User.last
   end
 
   def logged_in?
