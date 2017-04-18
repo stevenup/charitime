66e3e45575b5c6b0c589c73c0fe7d2644adce5cf
NaiveCAI <835010809@qq.com>
2017-04-18 23:28:03 +0800

    FNCR - Fuck off the sb KD logistics api bug

--- lib/modules/kdniao.rb
+++ lib/modules/kdniao.rb
@@ -5,6 +5,9 @@ require 'digest'
 module Modules::Kdniao
 
   def sign_data(req_data)
+    ## NOTE:
+    # Sb logistic API use ' in json API /wx
+    req_data = req_data.to_json.gsub('"', "'")
     md5_str = Digest::MD5.hexdigest("#{req_data}#{Settings.kdniao.APIKey}")
     puts '$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'
     encoded = Base64.encode64(md5_str)
