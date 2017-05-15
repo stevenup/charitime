3a0950bac61a294c4d41d8d7836c138d00470fe4
stevenup <289979456@qq.com>
2017-05-15 15:53:06 +0800

    BGFX - Fixed the problem that caused failure to sum up support.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -8,24 +8,26 @@ class ProjectsController < BaseController
     @project = Project.find_by_project_id(id)
     @shelf_items = ShelfItem.where('project_id = ? and is_on_shelf = ?', id, '1')
 
-    @total, @num_dedication_support, @num_purchase_support, @percent = 0, 0, 0, 0
+    @total, @num_dedication_support, @num_purchase_support, @percent, @support_count, total_dedication_support = 0, 0, 0, 0, 0, 0
     @price_gap = '498'
     @supports  = Support.where('project_id = ? and status = ?', id, '1')
 
     if @supports != []
       @supports.each do |ele|
         if ele.money and ele.support_type == '0'
-          @total                  += ele.money
-          @num_dedication_support += 1
-        elsif ele.order_detail_id and ele.support_type == '1'
-          @total                  += ele.order_detail.gyb_discount.to_f / 100
-          @num_purchase_support   += 1
+          @total                    += ele.money
+          total_dedication_support  += ele.money
+          @num_dedication_support   += ele.count
+          @support_count            += (total_dedication_support / 49800).to_i
+        elsif ele.support_type == '1'
+          @total                    += ele.money
+          @num_purchase_support     += ele.count
+          @support_count            += @num_purchase_support
         end
       end
       # @percent = @total.to_f / @project.goal.to_f
-      @price_gap = 498 - (@total.to_f % 498.0)
-      @percent   = (@total.to_f % 498.0).to_f / 498.0
+      @price_gap = 498 - (total_dedication_support.to_f % 49800.0) / 100
+      @percent   = (total_dedication_support % 49800.0).to_f / 49800.0
     end
-
   end
 end
